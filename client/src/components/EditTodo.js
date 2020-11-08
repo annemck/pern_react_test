666185449f5084d32f588c685e315a6a708bd676
annemck <annekh88@gmail.com>
2020-11-08 16:56:05 +0000

    todo_id state passing to modal

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,34 +1,36 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 
-const EditTodo = () => {
+const EditTodo = ({todo}) => {
+  
+  const [description, setDescription] = useState(todo.description);
   
   return (
     <Fragment>
       {/* Button to Open the Modal*/}
-      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+      <button type="button" className="btn btn-primary" data-toggle="modal" data-target={`#id${todo.todo_id}`}>
         Edit
       </button>
 
       {/* The Modal*/}
-      <div class="modal" id="myModal">
-        <div class="modal-dialog">
-          <div class="modal-content">
+      <div className="modal" id={`id${todo.todo_id}`}>
+        <div className="modal-dialog">
+          <div className="modal-content">
 
             {/* Modal Header*/}
-            <div class="modal-header">
-              <h4 class="modal-title">Edit Todo</h4>
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <div className="modal-header">
+              <h4 className="modal-title">Edit Todo</h4>
+              <button type="button" className="close" data-dismiss="modal">&times;</button>
             </div>
 
             {/* Modal body*/}
-            <div class="modal-body">
-              <input type="text" className="form-control"/>
+            <div className="modal-body">
+              <input type="text" className="form-control" value={description}/>
             </div>
 
             {/* Modal footer*/}
-            <div class="modal-footer">
-            <button type="button" class="btn btn-primary" data-dismiss="modal">Edit</button>
-              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            <div className="modal-footer">
+            <button type="button" className="btn btn-primary" data-dismiss="modal">Edit</button>
+              <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
             </div>
 
           </div>
