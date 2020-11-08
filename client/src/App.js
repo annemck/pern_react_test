4ecf1bb7a6daa2bc00b4d7935d49e8ca88bb8fc8
annemck <annekh88@gmail.com>
2020-11-08 15:10:54 +0000

    can add to db from front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React, {Fragment} from "react";
 import './App.css';
 
+//components
+import InputToDo from "./components/InputTodo.js";
+
 function App() {
-  return <Fragment></Fragment>;
+  return (
+  <Fragment>
+    <div className="container">
+      <InputToDo/>
+    </div>
+  </Fragment>
+  );
 }
 
 export default App;
