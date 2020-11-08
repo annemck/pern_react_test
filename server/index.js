5966acd8372b516ba3682e18a9a6959e7a427a91
annemck <annekh88@gmail.com>
2020-11-08 12:22:01 +0000

    can get single todo

--- server/index.js
+++ server/index.js
@@ -36,6 +36,18 @@ app.get("/todos", async(req, res) => {
 });
 
 //get a todo
+app.get("/todos/:id", async(req, res) => {
+  try {
+    
+    const {id} = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+    
+    res.json(todo.rows[0]);
+    
+  } catch (err){
+    console.log(err.message);
+  }
+});
 
 //update a todo
 
