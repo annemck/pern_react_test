ab3724e194a7c5b817a4e7fe95557ff301b18156
annemck <annekh88@gmail.com>
2020-11-08 11:53:44 +0000

    connection to db setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db.js");
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//routes
+//create a todo
+
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
