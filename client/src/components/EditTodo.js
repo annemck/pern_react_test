b6d4b7f809fc8a7c5a343c23c4a727634966b415
annemck <annekh88@gmail.com>
2020-11-08 17:09:47 +0000

    closing modal with no edit can be reopened with correct description

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -38,7 +38,7 @@ const EditTodo = ({todo}) => {
             {/* Modal Header*/}
             <div className="modal-header">
               <h4 className="modal-title">Edit Todo</h4>
-              <button type="button" className="close" data-dismiss="modal">&times;</button>
+              <button type="button" className="close" data-dismiss="modal" onClick={() => setDescription(todo.description)}>&times;</button>
             </div>
 
             {/* Modal body*/}
@@ -49,7 +49,7 @@ const EditTodo = ({todo}) => {
             {/* Modal footer*/}
             <div className="modal-footer">
             <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={e => updateDescription(e)}>Edit</button>
-              <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={() => setDescription(todo.description)}>Close</button>
             </div>
 
           </div>
