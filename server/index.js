e576378e5c2f345276eeffbcb7a8ded2accdafc9
annemck <annekh88@gmail.com>
2020-11-08 12:15:42 +0000

    can get all todos

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.use(express.json()); //req.body
 //create a todo
 app.post("/todos", async(req, res) => {
   try {
+    
     const {description} = req.body;
     const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
     
@@ -19,9 +20,20 @@ app.post("/todos", async(req, res) => {
   } catch (err) {
     console.log(err.message);
   }
-})
+});
 
 //get all todos
+app.get("/todos", async(req, res) => {
+  try {
+    
+    const allTodos = await pool.query("SELECT * FROM todo");
+    
+    res.json(allTodos.rows);
+    
+  } catch {
+    console.log(err.message);
+  }
+});
 
 //get a todo
 
